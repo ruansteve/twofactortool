10636ee0cfb15b3dfd8f4ec3ff27f9d28537c476
Shixin Ruan <shixin.ruan@zstack.io>
2020-04-08 12:11:49 +0800

    add para

--- prog.go
+++ prog.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	//"crypto"
+	"encoding/base32"
+	"flag"
 	"fmt"
 	"github.com/gwwfps/onetime"
-	"encoding/base32"
-	"io/ioutil"
-	"os"
+
+	//"github.com/gwwfps/onetime"
+	//"encoding/base64"
+	//"github.com/sec51/twofactor"
 )
 
+var secret  string
+
+func parseCommandOptions() {
+	flag.StringVar(&secret, "secret", "", "secret to generate code")
+}
+
 func main() {
+	parseCommandOptions()
 	var otp,_  = onetime.Simple(6)
-	str := readStdin()
-	data, err := base32.StdEncoding.DecodeString(str)
+	data, err := base32.StdEncoding.DecodeString(secret)
 	if (err == nil){
 		var code = otp.TOTP(data)
 		fmt.Println(code)
 	}else{
 		fmt.Println("error");
 	}
-}
-
-func readStdin() string{
-    bytes, _ := ioutil.ReadAll(os.Stdin)
-	return string(bytes)
 }
